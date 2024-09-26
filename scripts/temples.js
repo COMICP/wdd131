1ff00cc8b35625f34607687c766ca7de1482cc60
COMICP <binettejaden@gmail.com>
2024-09-26 18:25:51 -0400

    Basic Java

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+function drop() {
+    let x = document.getElementById("nav");
+    let but = document.getElementById('navbut')
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+        but.innerHTML = 'X';
+    } else {
+        x.style.display = "none";
+        but.innerHTML = '☰';
+    }
+}
